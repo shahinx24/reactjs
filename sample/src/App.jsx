85732196fcfc9100ae3084ef63618fc8777c8855
Shahin <shahinu886@gmail.com>
2025-12-19 16:20:19 +0530

    update

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -1,15 +1,15 @@
 import {useState} from "react";
 
 function App(){
-        // const [task,setTask] = useState([])
+        const [task,setTask] = useState([])
         const [name , setName] = useState("");
         const [age , setAge] = useState("");
         const [show,setShow] = useState(false)
 
 
-        // function Pop(){
-        //   setTask([name,age])
-        // }
+        function Pop(){
+          setTask([name,age])
+        }
         
         
         return(
@@ -18,8 +18,8 @@ function App(){
           <input value={name} onChange={(t)=> setName(t.target.value)}/>
           <p>Enter your age :</p>
           <input value={age} onChange={(t)=> setAge(t.target.value)}/>
-          <button onClick={()=> setShow(!show)}>Click</button>
-          {show && <p>{name} {age}</p>}
+          <button onClick={Pop}>Click</button>
+          <p>{task}</p>
           </div>
         );
 }
